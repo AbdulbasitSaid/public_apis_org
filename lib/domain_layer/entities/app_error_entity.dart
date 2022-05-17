99379c43c9b58adee0ead1705336783aa93cd353
Abdulbasit <abdulbasitsaeed619@gmail.com>
2022-05-17 23:48:58 +0100

    created AppError

---
+++ lib/domain_layer/entities/app_error_entity.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+
+class AppError extends Equatable {
+  final AppErrorType appErrorType;
+
+  const AppError({required this.appErrorType});
+  @override
+  List<Object?> get props => [appErrorType];
+}
+
+enum AppErrorType {
+  api,
+  network,
+  database,
+  unauthorized,
+  badRequest,
+  forbidden,
+  unProcessableEntity,
+  unExpected,
+  notFound,
+  serveError,
+  serverNotAvailble,
+}
